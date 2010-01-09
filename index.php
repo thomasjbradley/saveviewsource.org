e6a94475d4391e54cc91c84fc1951a11d5dd308e
Thomas J Bradley <theman@thomasjbradley.ca>
2010-01-08 20:55:23 -0500

    Initial import

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+	ob_start('ob_gzhandler');
+?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Save View Source · Join the Save View Source movement to save view source! · #saveviewsource</title>
+<!--
+I, View Source am so happy that you used me!
+I have served you very well in the past and hope you will graciously pledge to save me!
+Please continue to use me for your bidding, as I, View Source, am your humble servant.
+
+Tweet: #saveviewsource
+-->
+	<meta name="description" content="View source is an extremely important part of the web but its existence is being threatened. Join the Save View Source movement to save view source!">
+	<meta name="author" content="Thomas J Bradley">
+	<link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<meta name="viewport" content="width=1000">
+	<style type="text/css">
+	@font-face { font-family:"Museo"; src:url("fonts/Museo-300.eot"); src:local("Museo 300"), local("Museo-300"), url("fonts/Museo-300.woff") format("woff"), url("fonts/Museo-300.otf") format("opentype"); }
+	@font-face { font-family:"Museo"; font-weight: 700; src:url("fonts/Museo-700.eot"); src:local("Museo 700"), local("Museo-700"), url("fonts/Museo-700.woff") format("woff"), url("fonts/Museo-700.otf") format("opentype"); }
+	@font-face { font-family:"Museo"; font-weight: 500; src:url("fonts/Museo-500.eot"); src:local("Museo 500"), local("Museo-500"), url("fonts/Museo-500.woff") format("woff"), url("fonts/Museo-500.otf") format("opentype"); }
+	body { margin:0; padding:0; background-image: ; font:normal 100.01%/1.375 "Museo","Helvetica Neue",Helvetica,Arial,Corbel,sans-serif; }
+	img { margin:0; padding:0; border:0; }
+	a, a:link, a:visited { border-bottom: 0.1em solid #4242f0; color:#4242f0; text-decoration: none; }
+	a:hover, a:focus, a:active { color: #00e; }
+	ul { margin:0 0 1.375em; padding:0;}
+	.section { margin:0 auto; padding:0 13em; width:35em; }
+	.footer { height:35px; margin:0; padding:5px 0; border-top:1px solid #ccc; }
+	h1 { margin:0.2em 0 0.4em; padding:0; font-size:5.375em; font-weight:500; line-height:1em; }
+	h1 span { display:block; font-weight:700; }
+	h2 { margin:1em 0; padding:0; font-size:2.25em; }
+	p { margin:0 0 1.375em; padding:0; }
+	p.intro { font-size: 1.125em; }
+	p.save { font-size:1.5em; }
+	p.pledge { font-size: 1.875em; }
+	code { color:#e0e0e0; font-family: "Museo","Helvetica Neue",Helvetica,Arial,Corbel,sans-serif; text-align:right; }
+	code.flyout { margin-left:-3.2em; position:absolute; width:3em; }
+	p.save code.flyout { margin-left:-6.5em; }
+	code.doctype { display:block; margin: 1.375em 0 0; position:static; width:100%; font-size:1.875em; text-align:left; }
+	#tweets { min-height:2.75em; }
+	#tweets ul { margin:0; padding:0; list-style-type:none; }
+	#tweets li { clear:both; margin:0 0 1.375em -42px; min-height:1em; padding:0 0 0 42px; position:relative; }
+	#tweets li:after{ clear:both; display:block; height:0; overflow:hidden; visiblity:hidden; content:"."; font-size:0;}
+	#tweets code.flyout { left:-3.5em; margin-left:0; }
+	a.avatar, a.avatar:link, a.avatar:visited { display:inline; float:left; margin:0.4em 0 0 -42px; padding:0; border:0; }
+	#tweets div { display:inline; float:left; width:10em; }
+	#tweets p { display:inline; float:right; width: 23em; }
+	#tweets .time { display:block; }
+	a.thomasjbradley, a.thomasjbradley:link, a.thomasjbradley:visited { display:inline; float:right; height:35px; margin:0; overflow:hidden; padding:0; width:110px; background:transparent url("thomasjbradley.gif") no-repeat right top; border:0; text-indent:-999em; }
+	a.thomasjbradley:focus, a.thomasjbradley:hover, a.thomasjbradley:active { background-position:left top; }
+	</style>
+</head>
+<body>
+<div class="section">
+	<code class="doctype">&lt;!DOCTYPE html&gt;</code>
+	<h1><code class="flyout">&lt;h1&gt; </code>Save <span>View Source</span></h1>
+	<p class="intro"><code class="flyout">&lt;p&gt; </code>View source is an <code>&lt;em&gt;</code><em>extremely</em><code>&lt;/em&gt;</code> important part of the web but its existence is being threatened.
+	<p class="save"><strong><code class="flyout">&lt;p&gt;&lt;strong&gt; </code>Join the Save View Source movement to save view source!</strong>
+	<p class="pledge"><code class="flyout">&lt;p&gt; </code>Pledge your commitment to save view source by including the hash tag <strong><a href="http://twitter.com/home?status=I%20pledge%20to%20save%20view%20source!%20http://saveviewsource.org%20%23saveviewsource" title="Help save view source by tweeting your commitment to Save View Source!">#saveviewsource in a tweet</a></strong>!
+	<h2><code class="flyout">&lt;h2&gt; </code>Learn More About the Situation</h2>
+	<p>Some posts from the blog sphere about the forthcoming extinction of view source.
+	<ul>
+		<li><a href="http://ajaxian.com/archives/the-end-of-days-for-view-source" rel="external">The End of Days for “View Source”?</a>—Ajaxian
+		<li><a href="http://alex.dojotoolkit.org/2010/01/view-source-is-good-discuss/" rel="external">View-Source Is Good? Discuss.</a>—Alex Russell
+		<li><a href="http://ajaxian.com/archives/view-source-how-important-was-it-is-it-and-will-it-be" rel="external">View Source; How important was it, is it, and will it be?</a>—Ajaxian
+		<li><a href="http://thomasjbradley.ca/blog/save-view-source" rel="external">Save View Source</a>—Thomas J Bradley
+	</ul>
+	<h2><code class="flyout">&lt;h2&gt; </code>Recent Pledges</h2>
+	<div id="tweets"></div>
+	<p><code class="flyout">&lt;p&gt; </code><strong><a href="http://twitter.com/#search?q=%23saveviewsource">See all the other pledgers</a></strong>
+	<p class="pledge"><code class="flyout">&lt;p&gt; </code><strong><a href="http://twitter.com/home?status=I%20pledge%20to%20save%20view%20source!%20http://saveviewsource.org%20%23saveviewsource" title="Help save view source by tweeting your commitment to Save View Source!">tweet: #saveviewsource</a></strong>
+	<div class="footer"><a href="http://thomasjbradley.ca" rel="external" class="thomasjbradley" title="Designed and Developed by Thomas J Bradley">Thomas J Bradley</a></div>
+</div>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js" type="text/javascript"></script>
+<script src="jquery.juitter.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	$.Juitter.start({ searchType:'searchWord', searchObject:'saveviewsource', live:'live-60', placeHolder:'tweets', loadMSG:'Loading pledges…', total:10, openExternalLinks:'sameWindow' });
+});
+</script>
+<script type="text/javascript">
+var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-561679-3']); _gaq.push(['_trackPageview']);
+(function(){
+var ga = document.createElement('script');
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+ga.setAttribute('async', 'true');
+document.documentElement.firstChild.appendChild(ga);
+})();
+</script>
+</body>
+</html>
